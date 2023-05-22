f2ef9eb974dcb801688fe85cc8cd294a1b3806e9
Alexander <thelegindrum@gmail.com>
2023-05-22 10:30:17 +0300

    add commit

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,33 +1,47 @@
 const Form = () => {
-	return
 	const [formData, setFormData] = useState({
 		name: '',
 		email: '',
 		message: '',
 	})
-    
-    const handleChange = () => {
-        setFormData ({
-            ...formData,
-            [event.target.name]: event.target.value
-        })
-    }
-    (
+
+	const handleChange = (event) => {
+		setFormData({
+			...formData,
+			[event.target.name]: event.target.value,
+		})
+	}
+
+	return (
 		<form>
 			<label>
 				Name:
-				<input type="text" name="name" value={formData.name} onChange={handleChange} />
+				<input
+					type="text"
+					name="name"
+					value={formData.name}
+					onChange={handleChange}
+				/>
 			</label>
 			<label>
 				Email:
-				<input type="email" name="email" value="" onChange="" />
+				<input
+					type="email"
+					name="email"
+					value={formData.email}
+					onChange={handleChange}
+				/>
 			</label>
 			<label>
 				Message:
-				<textarea name="message" value="" onChange=""></textarea>
+				<textarea
+					name="message"
+					value={formData.message}
+					onChange={handleChange}
+				></textarea>
 			</label>
 			<button type="submit">Submit</button>
-		</form>,
+		</form>
 	)
 }
 
